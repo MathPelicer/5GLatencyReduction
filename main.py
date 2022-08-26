eb7f39f0acb2fb9c4209157ee3b9486476e37b44
MathPelicer <matheuspelicer58@gmail.com>
2022-08-26 20:36:22 -0300

    [code] workload creating based on capacity of cloud and fog nodes and the normal distribuition of the network usage during a day

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import utilities
 import json
 
-workload = json.loads(utilities.createWorkload(10))
+
+workload = json.loads(utilities.createWorkload(1, 2))
 
 distribution = utilities.createNormalDistribution()
-for i in range(len(distribution)):
-    if i % 1000 == 0:
-        print(str(i) + ": " + str(distribution[i] * 1000))
-
+# for i in range(len(distribution)):
+# if i % 1000 == 0:
+#print(str(i) + ": " + str(distribution[i] * 1000))
+print(workload)
 
 for device in workload:
     print(device)
-    print("id: " + str(device["device_id"]) + " class of service: " + device["class_of_service"])
+    # print("id: " + str(device["device_id"]) +
+    #      " class of service: " + device["class_of_service"])
